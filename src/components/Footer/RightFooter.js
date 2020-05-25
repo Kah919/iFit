a7cb2e5b5a0294b86b4c6446cc7aedec2768ebf5
Kah919 <kahyap13@gmail.com>
2020-05-25 04:02:59 -0400

    implement navbar hover animation

--- src/components/Footer/RightFooter.js
+++ src/components/Footer/RightFooter.js
@@ -5,7 +5,7 @@ import Language from './Language';
 export default () => {
     return(
         <div className={ styles.right_foot }>
-            <p> COPYRIGHT© iFit.com. Allrights reserved. </p>
+            <p> COPYRIGHT© iFit.com. Allrights reserved. &nbsp;</p>
             <Language />
         </div>
     )
