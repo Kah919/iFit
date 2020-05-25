e1af1aaea7747f26ecd90fcae6b9db7c93b88c99
Kah919 <kahyap13@gmail.com>
2020-05-25 05:40:08 -0400

    cards now have hover effect

--- src/components/Footer/LeftFooter.js
+++ src/components/Footer/LeftFooter.js
@@ -3,7 +3,6 @@ import styles from './Footer.module.css';
 import FootTab from './FootTab';
 import FootSocial from './FootSocial';
 
-
 export default () => {
     return(
         <div className={ styles.left_foot }>
--- src/components/Hero/HeroText.js
+++ src/components/Hero/HeroText.js
@@ -5,19 +5,19 @@ import Fade from 'react-reveal/Fade';
 export default () => {
     return(
         <Fade left>
-        <div className={ styles.text_container }>
-            <div> 
-                <span>WORKING OUT</span>
-                <br></br>
-                <span>HAS NEVER BEEN SO FUN</span>
-            </div>
+            <div className={ styles.text_container }>
+                <div> 
+                    <span>WORKING OUT</span>
+                    <br></br>
+                    <span>HAS NEVER BEEN SO FUN</span>
+                </div>
 
-            <p>
-                Welcome to the center of it all. The Fitness Shop 
-                presents you with hundreds of workout programs designed 
-                to guide your weight loss journey.
-            </p>
-        </div>
+                <p>
+                    Welcome to the center of it all. The Fitness Shop 
+                    presents you with hundreds of workout programs designed 
+                    to guide your weight loss journey.
+                </p>
+            </div>
         </Fade>
     )
 }
